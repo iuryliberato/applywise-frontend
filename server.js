058f5c1ee31fb8bfd6dcd69eead40b31c68d8e07
iuryliberato <iuryliberato@hotmail.com>
2025-12-03 14:11:08 +0100

    deployment frontend

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js (frontend)
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const distPath = path.join(__dirname, 'dist');
+
+app.use(express.static(distPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Frontend listening on port ${port}`);
+});
